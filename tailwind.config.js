cbace64740856cd38dbc7b9f01be44511fdc0d7f
theatjln <janicaliz.dev@gmail.com>
2022-02-06 18:50:18 +0800

    uninstalled tw-elements as an npm package, used it inside react-helmet component and updated nonotes/steps \(implementation guide\)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
+    "./components/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tw-elements/dist/plugin")],
+  plugins: [],
 };
